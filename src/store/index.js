fea15a8479aa241d658c092b105ab934becc5190
cp3 <792716300@qq.com>
2019-11-30 17:18:54 +0800

    登录状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},//状态
+  state: {
+    isLogin: false
+  },//状态,变量
   mutations: {},//包含多个更新state函数得对象
   actions: {},//包含多个对应事件回调函数得对象
-  modules: {}
+  modules: {},
+  getters: {}
 });
