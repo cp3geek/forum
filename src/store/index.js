4c00c855fe56b847ed46f0573927d0a4d5904e67
cp3 <792716300@qq.com>
2019-11-30 17:27:47 +0800

    nothing

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    isLogin: false
-  },//状态,变量
+  state: {},//状态,变量
   mutations: {},//包含多个更新state函数得对象
   actions: {},//包含多个对应事件回调函数得对象
   modules: {},
